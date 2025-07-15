3ac59b333dc0224610fc85f93753ca805bb7a03f
RichVegafresh <brightagyapong76@gmail.com>
2025-07-15 15:40:30 +0400

    Update Learningjurney.py

--- Learningjurney.py
+++ Learningjurney.py
@@ -2,7 +2,18 @@
 Learning journey
 #write a program to calculate the Area of a rectangle
 import math
-length = float(input("Enter the length of the rectangle:, "))
-width = float(input("Enter the value of the rectangle:, "))
+while True:
+  try:
+    length = float(input("Enter the length of the rectangle:, "))
+    break
+  except ValueError:
+    print("Please enter a valid number")
+while True:
+  try:
+    width = float(input("Enter the value of the rectangle:, "))
+    break
+  except ValueError:
+    print("Please enter a valid number")
+
 Area = (length * width)
 print("The Area of the Rectangle is:", Area)

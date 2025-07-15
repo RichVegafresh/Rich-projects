58f8f4360a494d279846f74593f0dc8ea5a35ff4
RichVegafresh <brightagyapong76@gmail.com>
2025-07-15 15:32:34 +0400

    Update Learningjurney.py

--- Learningjurney.py
+++ Learningjurney.py
@@ -1,2 +1,8 @@
 # Rich-projects
 Learning journey
+#write a program to calculate the Area of a rectangle
+import math
+length = float(input("Enter the length of the rectangle:, "))
+width = float(input("Enter the value of the rectangle:, "))
+Area = (length * width)
+print("The Area of the Rectangle is:", Area)
